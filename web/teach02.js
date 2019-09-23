6f16b7b02d51dcfe14adf53f559ab3285010eeae
Jake Green <jgreen47@byui.edu>
2019-09-23 12:38:15 -0500

    modified teach02.css .html .js

--- web/teach02.js
+++ web/teach02.js
@@ -1,3 +1,7 @@
 function clickAlert() {
   window.alert("Clicked!");
 }
+function changeColor() {
+  var color = document.getElementById('color').value;
+  document.getElementById('box1').style.color = color;
+}
