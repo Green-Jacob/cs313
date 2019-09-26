5f405de433acdcd9902b0d47588e6878384ddfaa
Jake Green <jgreen47@byui.edu>
2019-09-26 14:37:18 -0500

    modified home.php

--- web/home.php
+++ web/home.php
@@ -44,7 +44,7 @@
         <p>My wife and I enjoy board games. So I decided to make a dice roller.</p>
         <form action="home.php" method="get">
           Number of sides: <input type="number" name="sides" value=""><br>
-        <button type="button" name="roll">Roll</button>
+        <input type="submit" name="" value="Roll">
         </form>
         <?php
           $sides = $_GET["sides"];
