4b3ccaa009893652194f345f22d7197aa5a9fc0e
Jake Green <jgreen47@byui.edu>
2019-09-26 13:59:28 -0500

    modified home.php

--- web/home.php
+++ web/home.php
@@ -43,7 +43,8 @@
         <p>My wife and I enjoy board games. So I decided to make a dice roller.</p>
         <form action="home.php" method="get">
           Number of sides: <input type="number" name="sides" value=""><br>
-          <input type="submit" value="Roll" id="input" onclick="return setValue(false);">
+          <input type="submit" value="Roll" id="input"
+          onclick="return setValue(false);return false">
         </form>
         <?php echo $result; ?>
       </div>
