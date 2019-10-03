8e7db302eae34d79506289b014750ddfe1661528
Jake Green <jgreen47@byui.edu>
2019-10-03 12:17:12 -0500

    modified

--- web/prove03/catalog.php
+++ web/prove03/catalog.php
@@ -1,9 +1,12 @@
+<?php
+session_start();
+ ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jake Green's Homepage</title>
+    <title>Catalog of Amazing Things</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
     crossorigin="anonymous">
@@ -27,7 +30,7 @@
         </div>
       </div>
       <div class="box-large font-yanone">
-
+        
       </div>
       <footer>
         <a href="cart.php">cart</a><br>
