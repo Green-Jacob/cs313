fce5f461f981d3d97247e42acadb6766716310e7
Jake Green <jgreen47@byui.edu>
2019-10-03 11:57:18 -0500

    added prove03

---
+++ web/prove03/catalog.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jake Green's Homepage</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="./master.css">
+    <link href="https://fonts.googleapis.com/css?family=Acme|Yanone+Kaffeesatz&display=swap"
+    rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <a href="home.php"><img src="JakeGreenLogo.jpg" alt="Jake Green" id="logo"></a>
+        <h1>Jake <span id="green-text">Green</span>'s Homepage</h1>
+      </header>
+      <div class="sidebar">
+        <div class="dropdown">
+          <span>Pages</span>
+          <div class="dropdown-list">
+            <a href="home.php">Home</a><br>
+            <a href="assignments.html">Jake's Assignments</a>
+          </div>
+        </div>
+      </div>
+      <div class="box-large font-yanone">
+
+      </div>
+    </div><!-- end container -->
+  </body>
+</html>
