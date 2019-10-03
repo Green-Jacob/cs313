2cd03e3f984bc45d755439f2cefda34536f3dee5
Jake Green <jgreen47@byui.edu>
2019-10-03 15:25:51 -0500

    modified

--- web/prove03/session.php
+++ web/prove03/session.php
@@ -2,7 +2,7 @@
 session_start();
 //session_unset();
 $names = array_keys($_GET);
-$value = extract($_GET);
+$value = array_values($_GET);
 $name = $names[0];
 echo "<p> added ".$name."</p>";
 $_SESSION[$name] = $value[0];
