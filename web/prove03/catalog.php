936d2f736a69e289f04ebca635363bdbb6eff0bf
Jake Green <jgreen47@byui.edu>
2019-10-03 11:59:29 -0500

    modified

--- web/prove03/catalog.php
+++ web/prove03/catalog.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
     crossorigin="anonymous">
-    <link rel="stylesheet" href="./master.css">
+    <link rel="stylesheet" href="../master.css">
     <link href="https://fonts.googleapis.com/css?family=Acme|Yanone+Kaffeesatz&display=swap"
     rel="stylesheet">
   </head>
