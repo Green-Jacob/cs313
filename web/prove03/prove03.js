a004fa657a01315323dab89b73003a847637ebf0
Jake Green <jgreen47@byui.edu>
2019-10-03 15:43:57 -0500

    modified

--- web/prove03/prove03.js
+++ web/prove03/prove03.js
@@ -11,7 +11,7 @@
       //$("#session_write").load(url);
       $("#session_write").load(url, data, function(responseTxt){
         console.log(responseTxt);
-        $("#session_write").html(responseTxt);
+        $(this).html(responseTxt);
       });
     });//end button.click()
   });//end document.ready()
