53bbe08d8b6d40b9ba24058fd2e3cf20051c340d
Jake Green <jgreen47@byui.edu>
2019-10-03 15:11:40 -0500

    modified

--- web/prove03/session.php
+++ web/prove03/session.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
 //session_unset();
-$_SESSION["high-five"] = $_GET["highfive"];
+$names[] = array_keys($_GET);
+$name = names[0];
+$_SESSION[$name] = $_GET[0];
+//$_SESSION["high-five"] = $_GET["highfive"];
 // debug
-echo "<p> added ".($_SESSION["high-five"])."</p>";
+echo "<p> added ".($_SESSION["highfive"])."</p>";
  ?>
