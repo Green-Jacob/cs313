6d9a8fa62974500c49704d2123dcc0f2510716f2
Jake Green <jgreen47@byui.edu>
2019-10-03 14:28:37 -0500

    modified

--- web/prove03/session.php
+++ web/prove03/session.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
-if (isset($_GET['high-five'])) {
-  $_SESSION['high-five'] = $_GET['high-five'];
-}
+$_SESSION['high-five'] = $_GET['high-five'];
 if (isset($_SESSION['high-five'])) {
   echo "<p>"."{$_SESSION['high-five']}"."</p>";
 }
