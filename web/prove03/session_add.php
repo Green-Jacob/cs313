5d5d26b7fc1f2ebdabe661a1aeb2ca3796162d2b
Jake Green <jgreen47@byui.edu>
2019-10-04 13:32:43 -0500

    modified

--- web/prove03/session_add.php
+++ web/prove03/session_add.php
@@ -2,10 +2,8 @@
 session_start();
 $names = array_keys($_GET);
 $values = array_values($_GET);
-if (strcmp($names[0], "address") ) {
-  $name = htmlspecialchars_decode($names[0]);
-  $value = htmlspecialchars_decode($values[0]);
-}
+$name = htmlspecialchars_decode($names[0]);
+$value = htmlspecialchars_decode($values[0]);
 $_SESSION[$name] = $value;
 echo "added ".($_SESSION[$name]);
  ?>
