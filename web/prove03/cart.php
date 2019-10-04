2b1a69c14280bd8aa514636e4e62d803a6506704
Jake Green <jgreen47@byui.edu>
2019-10-04 13:50:00 -0500

    modified

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -38,10 +38,16 @@ $values = array_values($_SESSION);
       </div>
       <div class="box-large font-yanone">
         <?php
-        foreach ($_SESSION as $key => $value) {
-          if ($key == "handshake" || $key == "highfive") {
-            $button = "<button class=\"removeItem\" type=\"button\" name=\"".$key."\" value=\"".$value."\">".$key." $".$value.".00 Click to Remove</button>";
-            echo "$button"."<br>";
+        if (empty($_SESSION)) {
+          echo "Nothing in the cart.";
+          echo "<a href=\"catalog.php\">Return to catalog</a>";
+        }
+        else {
+          foreach ($_SESSION as $key => $value) {
+            if ($key == "handshake" || $key == "highfive") {
+              $button = "<button class=\"removeItem\" type=\"button\" name=\"".$key."\" value=\"".$value."\">".$key." $".$value.".00 Click to Remove</button>";
+              echo "$button"."<br>";
+            }
           }
         }
          ?>
