a2b1b910949ddc8cd4892ba1c03d6d6150fcd8b9
Jake Green <jgreen47@byui.edu>
2019-10-04 21:34:09 -0500

    modified

--- web/prove03/clean_session.php
+++ web/prove03/clean_session.php
@@ -1,5 +1,10 @@
 <?php
-session.unset();
-session.destroy();
+session_start();
+$names = array_keys($_GET);
+$value = array_values($_GET);
+for ($i=0; $i < count($_SESSION); $i++) {
+  $name = $names[i];
+  unset($_SESSION[$name]);
+}
 echo "Cleared Session";
  ?>
