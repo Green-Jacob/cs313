9d6a032bae20d71e28499dd19807a6ffe039b149
Jake Green <jgreen47@byui.edu>
2019-10-04 20:48:13 -0500

    modified

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -45,7 +45,8 @@ $values = array_values($_SESSION);
         else {
           foreach ($_SESSION as $key => $value) {
             if ($key == "Handshake" || $key == "Hug") {
-              $button = "<button class=\"removeItem\" type=\"button\" name=\"".$key."\" value=\"".$value."\">".$key." $".$value.".00 Click to Remove</button>";
+              $item = "Item: ".$key." Price: $".$value.".00";
+              $button = "<button class=\"removeItem\" type=\"button\" name=\"".$key."\" value=\"".$value."\">Click to Remove</button>";
               echo "$button"."<br>";
             }
           }
