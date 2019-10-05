f3e22fde8dc376475810cb8286c3c66ad29b01d1
Jake Green <jgreen47@byui.edu>
2019-10-04 21:36:18 -0500

    modified

--- web/prove03/clean_session.php
+++ web/prove03/clean_session.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $names = array_keys($_SESSION);
-for ($i=0; $i < count($_SESSION); $i++) {
+for ($i=0; $i < count($names); $i++) {
   $name = $names[i];
   unset($_SESSION[$name]);
 }
