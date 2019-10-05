131f5eb85c2a1b30c61d8dc4c182c88b2c4e9a94
Jake Green <jgreen47@byui.edu>
2019-10-04 21:06:19 -0500

    modified

--- web/prove03/session_add.php
+++ web/prove03/session_add.php
@@ -4,6 +4,7 @@ $names = array_keys($_GET);
 $values = array_values($_GET);
 $name = htmlspecialchars_decode($names[0]);
 $value = htmlspecialchars_decode($values[0]);
+$value = strip_tags($value);
 $_SESSION[$name] = $value;
 echo "added ".($_SESSION[$name]);
  ?>
