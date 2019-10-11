78e1077d4c236796af799a62b18c084662f37e7b
Jake Green <jgreen47@byui.edu>
2019-10-11 16:52:35 -0500

    modified test.sql

--- db/test.sql
+++ db/test.sql
@@ -1,14 +1,1 @@
-CREATE TABLE test(
-  number real,
-  id SERIAL NOT NULL
-  );
-
-CREATE TABLE test1(
-  number real
-  );
-
-INSERT INTO test VALUES(1);
-
-INSERT INTO test1 VALUES(
-  (SELECT number FROM test WHERE id = 1) *3
-  );
+DROP SCHEMA seventh, seventh_honors, eighth, eighth_honors;
