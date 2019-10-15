0e2dc564c0a9e4171178097b8d1622580e21ec6a
Jake Green <jgreen47@byui.edu>
2019-10-15 12:21:21 -0500

    prepare05.php

--- web/prepare05.php
+++ web/prepare05.php
@@ -28,4 +28,10 @@ foreach ($db->query('SELECT username, password FROM note_user') as $row)
   echo ' password: ' . $row['password'];
   echo '<br/>';
 }
+
+$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
  ?>
