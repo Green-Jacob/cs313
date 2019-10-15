734ba3893469247a475ea0be190818c8f15cad52
Jake Green <jgreen47@byui.edu>
2019-10-15 12:46:34 -0500

    test.sql

--- db/test.sql
+++ db/test.sql
@@ -1,18 +1,18 @@
-INSERT INTO seventh.students(name, period) VALUES('Samuel', 1);
-INSERT INTO seventh.students(name, period) VALUES('Hannah', 1);
-INSERT INTO seventh.students(name, period) VALUES('Rick', 1);
+INSERT INTO seventh.assignments(name, total_score) VALUES('Assignment 4-Order of operations', 10);
+INSERT INTO seventh.assignments(name, total_score) VALUES('Assignment 2-division', 15);
+INSERT INTO seventh.assignments(name, total_score) VALUES('Assignment 3-decimals', 7);
 
-INSERT INTO seventh_honors.students(name, period) VALUES('Jonah', 2);
-INSERT INTO seventh_honors.students(name, period) VALUES('Gerson', 2);
-INSERT INTO seventh_honors.students(name, period) VALUES('Rachel', 2);
-INSERT INTO seventh_honors.students(name, period) VALUES('Robert', 2);
+INSERT INTO seventh_honors.assignments(name, total_score) VALUES('Assignment 1-factoring', 10);
+INSERT INTO seventh_honors.assignments(name, total_score) VALUES('Assignment 2-angles', 6);
+INSERT INTO seventh_honors.assignments(name, total_score) VALUES('Assignment 3-measuring angles', 5);
+INSERT INTO seventh_honors.assignments(name, total_score) VALUES('Assignment 4-supplementary', 5);
 
-INSERT INTO eighth.students(name, period) VALUES('Jackson', 4);
-INSERT INTO eighth.students(name, period) VALUES('Adam', 4);
-INSERT INTO eighth.students(name, period) VALUES('Aaron', 4);
-INSERT INTO eighth.students(name, period) VALUES('Zebe', 4);
+INSERT INTO eighth.assignments(name, total_score) VALUES('Assignment 1-order of operations', 10);
+INSERT INTO eighth.assignments(name, total_score) VALUES('Assignment 2-angles', 6);
+INSERT INTO eighth.assignments(name, total_score) VALUES('Assignment 3-measuring angles', 5);
+INSERT INTO eighth.assignments(name, total_score) VALUES('Assignment 4-supplementary', 5);
 
-INSERT INTO eighth_honors.students(name, period) VALUES('Fredrick', 5);
-INSERT INTO eighth_honors.students(name, period) VALUES('Jaun', 5);
-INSERT INTO eighth_honors.students(name, period) VALUES('Paula', 5);
-INSERT INTO eighth_honors.students(name, period) VALUES('Brianna', 5);
+INSERT INTO eighth_honors.assignments(name, total_score) VALUES('Assignment 1-binomials', 10);
+INSERT INTO eighth_honors.assignments(name, total_score) VALUES('Assignment 2-binomials 2', 8);
+INSERT INTO eighth_honors.assignments(name, total_score) VALUES('Assignment 3-simplify binomials', 12);
+INSERT INTO eighth_honors.assignments(name, total_score) VALUES('Assignment 4-project', 20);
