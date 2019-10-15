caa9cdd711ac6863973d2840be1a2f69f5141033
Jake Green <jgreen47@byui.edu>
2019-10-15 12:47:45 -0500

    test.sql

--- db/test.sql
+++ db/test.sql
@@ -4,7 +4,7 @@ INSERT INTO seventh.assignments(name, total_score) VALUES('Assignment 3-decimals
 
 INSERT INTO seventh_honors.assignments(name, total_score) VALUES('Assignment 1-factoring', 10);
 INSERT INTO seventh_honors.assignments(name, total_score) VALUES('Assignment 2-angles', 6);
-INSERT INTO seventh_honors.assignments(name, total_score) VALUES('Assignment 3-measuring angles', 5);
+INSERT INTO seventh_honors.assignments(name, total_score) VALUES('Assignment 3-measuring', 5);
 INSERT INTO seventh_honors.assignments(name, total_score) VALUES('Assignment 4-supplementary', 5);
 
 INSERT INTO eighth.assignments(name, total_score) VALUES('Assignment 1-order of operations', 10);
@@ -14,5 +14,5 @@ INSERT INTO eighth.assignments(name, total_score) VALUES('Assignment 4-supplemen
 
 INSERT INTO eighth_honors.assignments(name, total_score) VALUES('Assignment 1-binomials', 10);
 INSERT INTO eighth_honors.assignments(name, total_score) VALUES('Assignment 2-binomials 2', 8);
-INSERT INTO eighth_honors.assignments(name, total_score) VALUES('Assignment 3-simplify binomials', 12);
+INSERT INTO eighth_honors.assignments(name, total_score) VALUES('Assignment 3-simplify binom', 12);
 INSERT INTO eighth_honors.assignments(name, total_score) VALUES('Assignment 4-project', 20);
