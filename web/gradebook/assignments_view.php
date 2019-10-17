64e6267ef3d2c58961ba2ac6a662e2081ab11f96
Jake Green <jgreen47@byui.edu>
2019-10-17 12:58:33 -0500

    added to assignments_view

--- web/gradebook/assignments_view.php
+++ web/gradebook/assignments_view.php
@@ -1,9 +1,13 @@
+<?php
+require 'dbConnect.php';
+$db = connect_db();
+ ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gradebook Online</title>
+    <title>Assignments</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
     crossorigin="anonymous">
@@ -14,7 +18,7 @@
   <body>
     <div class="container">
       <header>
-        Gradebook Online
+        Assignments
       </header>
       <div class="sidebar">
         <div class="dropdown">
@@ -28,12 +32,60 @@
             <a href="assignments_view.php">Assignments</a>
           </div>
         </div>
+        <div class="sidebar">
+          Function for adding assignments will go here
+        </div>
         <div class="dropdown">
           <a href="search.php">Search</a>
         </div>
       </div>
-      <div class="box-large">
-        
+      <div class="box-small">
+        <h1>Seventh Grade Assignments</h1><hr>
+        <?php
+        foreach ($db->query('SELECT name, period FROM seventh.assignments') as $row)
+        {
+          echo 'Name: ' . $row['name'];
+          echo '<br/>';
+          echo 'Period: ' . $row['period'];
+          echo '<br/><hr>';
+        }
+         ?>
+      </div>
+      <div class="box-small">
+        <h1>Seventh Grade Honors Assignments</h1><hr>
+        <?php
+        foreach ($db->query('SELECT name, period FROM seventh_honors.assignments') as $row)
+        {
+          echo 'Name: ' . $row['name'];
+          echo '<br/>';
+          echo 'Period: ' . $row['period'];
+          echo '<br/><hr>';
+        }
+         ?>
+      </div>
+      <div class="box-small">
+        <h1>Eighth Grade Assignments</h1><hr>
+        <?php
+        foreach ($db->query('SELECT name, period FROM eighth.assignments') as $row)
+        {
+          echo 'Name: ' . $row['name'];
+          echo '<br/>';
+          echo 'Period: ' . $row['period'];
+          echo '<br/><hr>';
+        }
+         ?>
+      </div>
+      <div class="box-small">
+        <h1>Eighth Grade Honors Assignments</h1><hr>
+        <?php
+        foreach ($db->query('SELECT name, period FROM eighth_honors.assignments') as $row)
+        {
+          echo 'Name: ' . $row['name'];
+          echo '<br/>';
+          echo 'Period: ' . $row['period'];
+          echo '<br/><hr>';
+        }
+         ?>
       </div>
     </div><!-- end container -->
   </body>
