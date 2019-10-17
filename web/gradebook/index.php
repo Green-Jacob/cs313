e39f07778489ad06ec84238ec30e6de5cc049fd7
Jake Green <jgreen47@byui.edu>
2019-10-17 12:17:28 -0500

    added placeholding files for project

---
+++ web/gradebook/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gradebook Online</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="../master.css">
+    <link href="https://fonts.googleapis.com/css?family=Acme|Yanone+Kaffeesatz&display=swap"
+    rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        Gradebook Online
+      </header>
+      <div class="sidebar">
+        <div class="dropdown">
+          <a href="../home.php">Home</a>
+        </div>
+        <div class="dropdown">
+          
+        </div>
+      </div>
+      <div class="box-large">
+        placeholder
+      </div>
+    </div><!-- end container -->
+  </body>
+</html>
