3596cf7026198a2caa5033c59f58a5f3d5e47dd6
Jake Green <jgreen47@byui.edu>
2019-10-17 12:37:41 -0500

    session_start() on pages that need it. Added to student_view.php

--- web/gradebook/assignments_view.php
+++ web/gradebook/assignments_view.php
@@ -2,9 +2,39 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gradebook Online</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="../master.css">
+    <link href="https://fonts.googleapis.com/css?family=Acme|Yanone+Kaffeesatz&display=swap"
+    rel="stylesheet">
   </head>
   <body>
-
+    <div class="container">
+      <header>
+        Gradebook Online
+      </header>
+      <div class="sidebar">
+        <div class="dropdown">
+          <a href="../home.php">Home</a>
+        </div>
+        <div class="dropdown">
+          <span>Gradebook pages</span>
+          <div class="dropdown-list">
+            <a href="gradebook_view.php">Graded entries</a>
+            <a href="student_view.php">Students</a>
+            <a href="assignments_view.php">Assignments</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <a href="search.php">Search</a>
+        </div>
+      </div>
+      <div class="box-large">
+        
+      </div>
+    </div><!-- end container -->
   </body>
 </html>
---
+++ web/gradebook/student_view.php
@@ -0,0 +1,73 @@
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  //echo $dbHost.$dbPort.$dbUser.$dbName;
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gradebook Online</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="../master.css">
+    <link href="https://fonts.googleapis.com/css?family=Acme|Yanone+Kaffeesatz&display=swap"
+    rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        Gradebook Online
+      </header>
+      <div class="sidebar">
+        <div class="dropdown">
+          <a href="../home.php">Home</a>
+        </div>
+        <div class="dropdown">
+          <span>Gradebook pages</span>
+          <div class="dropdown-list">
+            <a href="gradebook_view.php">Graded entries</a>
+            <a href="student_view.php">Students</a>
+            <a href="assignments_view.php">Assignments</a>
+          </div>
+        </div>
+        <div class="dropdown">
+          <a href="search.php">Search</a>
+        </div>
+      </div>
+      <div class="box-large">
+        <h1>Seventh Grade Students</h1>
+        <?php
+        foreach ($db->query('SELECT name, period FROM seventh.students') as $row)
+        {
+          echo 'name: ' . $row['name'];
+          echo '<br/>';
+          echo 'period: ' . $row['period'];
+          echo '<br/><hr>';
+        }
+         ?>
+      </div>
+    </div><!-- end container -->
+  </body>
+</html>
