fa402000e23af28f93931e6774764ed9a6e12d5f
Jake Green <jgreen47@byui.edu>
2019-10-22 12:36:36 -0500

    added skeleton php file for teach05

---
+++ web/teach06/updateScripture.php
@@ -0,0 +1,14 @@
+<?php
+require "../dbConnect.php";
+$db = connect_db();
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
