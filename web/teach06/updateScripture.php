9ebb93c7c363944e088543419338fe8b3c744732
Jake Green <jgreen47@byui.edu>
2019-10-22 19:27:02 -0500

    added all the things

--- web/teach06/updateScripture.php
+++ web/teach06/updateScripture.php
@@ -10,7 +10,7 @@ $db = connect_db();
   </head>
   <body>
     <?php
-    foreach ($db->query('SELECT book, chapter, verse, content FROM public.scripture') as $row)
+    /*foreach ($db->query('SELECT book, chapter, verse, content FROM public.scripture') as $row)
     {
       echo 'Book: <b>' . $row['book'] . "</b>";
       echo '<br/>';
@@ -20,7 +20,21 @@ $db = connect_db();
       echo '<br/>';
       echo 'Content: <b>' . $row['content'] . "</b>";
       echo '<br/><hr>';
-    }
+    }*/
      ?>
+     <form class="" action="updateScriptures.php" method="post">
+       Book: <input type="text" name="book" value=""><br>
+       Chapter: <input type="text" name="chapter" value=""><br>
+       Verse: <input type="text" name="verse" value=""><br>
+       Content: <textarea name="content" rows="8" cols="80"></textarea><br>
+       <?php
+       foreach ($db->query('SELECT name FROM public.topic') as $row)
+       {
+        echo  '<input type="checkbox" name="topic[]" value="' . $row['name'] .'">';
+       }
+        ?>
+        <br>
+       <input type="submit" name="" value="Push it NOW!!!">
+     </form>
   </body>
 </html>
