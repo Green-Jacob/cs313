f59336da894fbbea1e878dd910eee47fe34a519b
Jake Green <jgreen47@byui.edu>
2019-10-25 19:51:14 -0500

    Yerp

--- web/gradebook/gradebook_modify.php
+++ web/gradebook/gradebook_modify.php
@@ -115,7 +115,7 @@ $db = connect_db();
             //$sc = $_POST['score'];
             $t;
             try {
-                foreach ($db->query("SELECT total_score FROM $c.assignments") as $row)
+                foreach ($db->query("SELECT total_score FROM $c.assignments WHERE name = $a") as $row)
                 {
                   $t = $row['total_score'];
                 }
